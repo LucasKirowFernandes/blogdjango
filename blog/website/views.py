7424d0314423b2c61805efb80be696b7726e1edf
LucasKF18 <lucasfernandesk18@gmail.com>
2020-12-12 16:35:25 -0300

    Aprofundando no sistema de Templates

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
 def hello_blog(request):
-    return render(request, 'index.html')
+    lista = [
+        'Django', 'Python', 'Git', 'Html', 
+        'Banco de Dados', 'Linux', 'Nginx', 'Uwsgi',
+        'Systemctl'
+    ]
+    data = {'name': 'Curso de Django 3', 'lista_tecnologias': lista}
+    return render(request, 'index.html', data)
